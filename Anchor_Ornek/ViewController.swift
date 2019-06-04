c119c4f0d4ee39cef1695d3fd4c4a689cd878a6f
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-06-04 19:21:29 +0300

    Ders 4-Anchor ile Auto Layout

--- Anchor_Ornek/ViewController.swift
+++ Anchor_Ornek/ViewController.swift
@@ -9,12 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    let kirmiziView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        return view
+    }()
+    let maviView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        return view
+    }()
+    let sariView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .yellow
+        return view
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(kirmiziView)
+        view.addSubview(maviView)
+        view.addSubview(sariView)
+        
+        kirmiziViewConstraint()
+        maviViewConstraint()
+        sariViewConstraint()
+        
+    }
+    
+    func kirmiziViewConstraint() {
+        kirmiziView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        kirmiziView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        kirmiziView.heightAnchor.constraint(equalToConstant: 110).isActive = true
+        kirmiziView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
+    
+    func maviViewConstraint() {
+        
+        
+        maviView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        maviView.widthAnchor.constraint(equalTo: kirmiziView.widthAnchor).isActive = true
+        maviView.heightAnchor.constraint(equalTo: kirmiziView.heightAnchor).isActive = true // mavi ve kırmızı view eşit yükseklikte
+        maviView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+    }
+    func sariViewConstraint() {
+        
+        sariView.trailingAnchor.constraint(equalTo: maviView.trailingAnchor).isActive = true
+        sariView.leadingAnchor.constraint(equalTo: kirmiziView.leadingAnchor).isActive = true
+        sariView.bottomAnchor.constraint(equalTo: kirmiziView.topAnchor, constant: -20).isActive = true
+        sariView.heightAnchor.constraint(equalTo: kirmiziView.heightAnchor, multiplier: 1.3).isActive = true
+        
+        sariView.topAnchor.constraint(greaterThanOrEqualTo: view.topAnchor, constant: 30).isActive = true
+        
+        
+    }
 
 }
 
